f7526920feb9d7cfcde45412a0d149f595996c0e
Logan Ferguson <lferg1292@yahoo.com>
2020-03-05 12:23:41 -0500

    Add project files.

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome(4554));
+            Console.ReadLine();
+        }
+
+        static bool IsPalindrome(int x)
+        {
+
+            string num = Convert.ToString(x);
+            int length = num.Length - 1;
+            //       1001
+            for (int i = 0; i < num.Length / 2; i++)
+            {
+                if (num[i] != num[length])
+                {
+                    return false;
+                }
+                length--;
+            }
+            return true;
+        }
+    }
+}
